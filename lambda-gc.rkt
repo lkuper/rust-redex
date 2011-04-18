d045141c89c2044640ffdbd97bb8d349621c12bf
Lindsey Kuper <lkuper@mozilla.com>
2011-04-18 16:42:12 -0700

    A Redex model of lambda-gc.
    Exactly what it says on the tin!  Tests seem to be passing.  Now that
    I have this working I'd like to go back and try to use the same idea
    to model the heap in Rust.

---
+++ lambda-gc.rkt
@@ -0,0 +1,97 @@
+;; A Redex model of the lambda-gc language from Morrisett, Felleisen,
+;; and Harper, "Abstract Models of Memory Management"
+;; (http://www.cs.cmu.edu/~rwh/papers/gc/fpca95.pdf).
+
+#lang racket
+(require redex)
+
+(define-language lambda-gc
+  ;; Programs.
+  (Var variable-not-otherwise-mentioned)
+  (Int number)
+  (Exp Var Int (pair Exp_1 Exp_2) (proj1 Exp) (proj2 Exp) (lambda (Var) Exp)
+       (Exp_1 Exp_2))
+  (Hval Int (pair Var_1 Var_2) (lambda (Var) Exp))
+  (Heap ((Var Hval) ...))
+  (Prog (letrec Heap in Exp))
+  (Ans (letrec Heap in Var))
+
+  ;; Evaluation contexts and instruction expressions.
+  (Ctxt hole (pair Ctxt Exp) (pair Var Ctxt) (proj1 Ctxt) (proj2 Ctxt)
+        (Ctxt Exp) (Var Ctxt))
+  (Instr Hval (proj1 Var) (proj2 Var) (Var Var))
+
+  ;; This is just so we can define the domain of 'project'.
+  (label proj1 proj2))
+
+(define lambda-gc-red
+  (reduction-relation
+   lambda-gc
+
+   (---> (letrec Heap in (in-hole Ctxt Hval))
+         ;; We use append rather than cons (here and in the app rule)
+         ;; to match the semantics of the paper, where new bindings
+         ;; get added at the end of the heap rather than the
+         ;; beginning.
+         (letrec ,(append (term Heap) `((,(term Var) ,(term Hval))))
+           in (in-hole Ctxt Var))
+         (where Var ,(variable-not-in (term Heap) (term Var)))
+         "alloc")
+   (---> (letrec Heap in (in-hole Ctxt (proj1 Var)))
+         (letrec Heap in (in-hole Ctxt (project proj1 Heap Var))) 
+         "proj1")
+   (---> (letrec Heap in (in-hole Ctxt (proj2 Var)))
+         (letrec Heap in (in-hole Ctxt (project proj2 Heap Var))) 
+         "proj2")
+   (---> (letrec Heap in (in-hole Ctxt (Var_1 Var_2)))
+         (letrec ,(append (term Heap) `((,(term Var_3) ,(term (heap-lookup Heap Var_2)))))
+           in (in-hole Ctxt Exp))
+         (where (lambda (Var_3) Exp) (heap-lookup Heap Var_1))
+         "app")
+   with
+   [(--> (in-hole Ctxt a) (in-hole Ctxt b)) (---> a b)]))
+
+(define-metafunction lambda-gc
+  project : label Heap Var -> Var
+  ;; second and third, instead of first and second, because pairs
+  ;; start with the tag 'pair.
+  [(project proj1 Heap Var) ,(second (term (heap-lookup Heap Var)))]
+  [(project proj2 Heap Var) ,(third (term (heap-lookup Heap Var)))])
+
+(define-metafunction lambda-gc
+  heap-lookup : Heap Var -> Hval
+  [(heap-lookup Heap Var) ,(second (assq (term Var) (term Heap)))])
+
+;; A few tests.
+
+(define (test-suite)
+  (test-->> lambda-gc-red
+            (term (letrec () in 3))
+            (term (letrec ((Var 3))
+                    in Var)))
+
+  (test-->> lambda-gc-red
+            (term (letrec () in (pair 3 4)))
+            (term (letrec ((Var 3)
+                           (Var1 4)
+                           (Var2 (pair Var Var1)))
+                    in Var2)))
+
+  (test-->> lambda-gc-red
+            (term (letrec () in (proj1 (pair 3 4))))
+            (term (letrec ((Var 3)
+                           (Var1 4)
+                           (Var2 (pair Var Var1)))
+                    in Var)))
+
+  ;; The example from the lambda-gc TR
+  ;; (http://www.cs.cmu.edu/~rwh/papers/gc/tr.pdf).
+  (test-->> lambda-gc-red
+            (term (letrec ((x 1)) in ((lambda (y) (proj1 y)) (pair x x))))
+            (term (letrec ((x 1)
+                           (Var (lambda (y) (proj1 y)))
+                           (Var1 (pair x x))
+                           (y (pair x x)))
+                    in x)))
+  
+  (test-results))
